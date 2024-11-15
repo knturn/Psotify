37c81fe46115bef52760baec507b4b9dfe2c541e
Kaan Turan <kaann.dev@gmail.com>
2024-11-15 06:18:13 +0300

    Initial Commit

---
+++ Psotify/PsotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PsotifyApp.swift
+//  Psotify
+//
+//  Created by Turan, Kaan on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PsotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
